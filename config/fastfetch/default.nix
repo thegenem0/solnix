795ccf109911936a78fce606231dd726711cd128
thegenem0 <nemethgergo02@gmail.com>
2024-10-15 14:15:09 +0100

    feat: update hyprland

--- config/fastfetch/default.nix
+++ config/fastfetch/default.nix
@@ -4,109 +4,30 @@
 
     settings = {
       display = {
-        color = {
-          keys = "35";
-          output = "90";
+        separator = " ==> ";
         };
-      };
 
       logo = {
-        source = ./nixos.png;
-        type = "kitty-direct";
-        height = 15;
-        width = 30;
-        padding = {
-          top = 3;
-          left = 3;
-        };
+        type = "auto";
       };
 
       modules = [
-          "break"
-          {
-              type = "custom";
-              format = "┌──────────────────────Hardware──────────────────────┐";
-          }
-          {
-              type = "cpu";
-              key = "│  ";
-          }
-          {
-              type = "gpu";
-              key = "│ 󰍛 ";
-          }
-          {
-              type = "memory";
-              key = "│ 󰑭 ";
-          }
-          {
-              type = "custom";
-              format = "└────────────────────────────────────────────────────┘";
-          }
-          "break"
-          {
-              type = "custom";
-              format = "┌──────────────────────Software──────────────────────┐";
-          }
-          {
-              type = "custom";
-              format = " OS -> ZaneyOS 2.2";
-          }
-          {
-              type = "kernel";
-              key = "│ ├ ";
-          }
-          {
-              type = "packages";
-              key = "│ ├󰏖 ";
-          }
-          {
-              type = "shell";
-              key = "└ └ ";
-          }
-          "break"
-          {
-              type = "wm";
-              key = " WM";
-          }
-          {
-              type = "wmtheme";
-              key = "│ ├󰉼 ";
-          }
-          {
-              type = "terminal";
-              key = "└ └ ";
-          }
-          {
-              type = "custom";
-              format = "└────────────────────────────────────────────────────┘";
-          }
-          "break"
-          {
-              type = "custom";
-              format = "┌────────────────────Uptime / Age────────────────────┐";
-          }
-          {
-              type = "command";
-              key = "│  ";
-              text = #bash
-              ''
-                birth_install=$(stat -c %W /)
-                current=$(date +%s)
-                delta=$((current - birth_install))
-                delta_days=$((delta / 86400))
-                echo $delta_days days
-              '';
-          }
-          {
-              type = "uptime";
-              key = "│  ";
-          }
-          {
-              type = "custom";
-              format = "└────────────────────────────────────────────────────┘";
-          }
-          "break"
+        "title",
+        "separator",
+        "os",
+        "host",
+        "kernel",
+        "uptime",
+        "packages",
+        "shell",
+        "wm",
+        "terminal",
+        "cpu",
+        "gpu",
+        "memory",
+        "disk",
+        "localip",
+        "break"
       ];
     };
   };
