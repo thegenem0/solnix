9890c8e50e041c95cee8564bd01093e50ebf6a8d
thegenem0 <nemethgergo02@gmail.com>
2024-10-16 13:52:38 +0100

    fix: remove version var

--- flake.nix
+++ flake.nix
@@ -2,7 +2,6 @@
   description = "solnix";
 
   inputs = {
-    hyprlandVersion = "v0.44.1";
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     home-manager.url = "github:nix-community/home-manager/master";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
