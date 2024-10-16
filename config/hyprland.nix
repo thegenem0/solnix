4f1e5b00f5e35e85a32abbd422b40869a2322f62
thegenem0 <nemethgergo02@gmail.com>
2024-10-16 23:57:36 +0100

    fix: should be string

--- config/hyprland.nix
+++ config/hyprland.nix
@@ -112,7 +112,7 @@ with lib;
           drop_shadow = true;
           shadow_range = 4;
           shadow_render_power = 3;
-          col.shadow = "rgba(1a1a1aee)";
+          "col.shadow" = "rgba(1a1a1aee)";
         };
         animations = {
           enabled = true;
