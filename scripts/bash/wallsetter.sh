7cc3c18ef3153039e538092a372c012215784701
thegenem0 <nemethgergo02@gmail.com>
2024-10-17 01:57:52 +0100

    fix: symlink hell

--- scripts/bash/wallsetter.sh
+++ scripts/bash/wallsetter.sh
@@ -1,48 +1,41 @@
 username=$(whoami)
-echo "username: $username"
-
 wall_dir="/home/${username}/Pictures/Wallpapers"
-rofi_theme="/home/${username}/.config/rofi/wallselect.rasi"
+rofi_theme="/home/${username}/.config/rofi/wallpapers.rasi"
 
-selected=$( find -L "$wall_dir" -type f \( -iname "*.jpg" -o -iname "*.jpeg" -o -iname "*.png" \) -exec basename {} \; | sort -R | while read rfile
-  do
-      echo -en "$rfile\x00icon\x1f$wall_dir/${rfile}\n"
-  done | rofi -dmenu -i -replace -config ${rofi_theme})
-  if [ ! "$selected" ]; then
-      echo "No wallpaper selected"
-      exit
-  fi
+selected=$(find "$wall_dir" -type f \( -iname "*.jpg" -o -iname "*.jpeg" -o -iname "*.png" \) -exec basename {} \; | sort -R | while read rfile
+    do
+        echo -en "$rfile\x00icon\x1f$wall_dir/$rfile\n"
+    done | rofi -dmenu -i -replace -config "$rofi_theme")
 
-if [ -n "$selected" ]; then
-  if pgrep -x "swww-daemon" > /dev/null; then
-      echo "swww-daemon is already running"
-  else
-      echo "Starting swww-daemon"
-      swww-daemon &
-      sleep 0.5
-  fi
-
-  if [ -L "/home/${username}/.config/.current_wallpaper" ]; then
-   rm "/home/${username}/.config/.current_wallpaper"
-  fi
-
-  ln -s "$selected" "/home/${username}/.config/.current_wallpaper"
-
-  blurred_wallpaper="/home/${username}/.config/.blurred_wallpaper"
-  if [ -f "$blurred_wallpaper" ]; then
-     rm "$blurred_wallpaper"
-  fi
-
-  magick "$selected" -blur 0x8 "$blurred_wallpaper"
-
-  # Set the wallpaper using swww
-  swww img "$selected" --transition-type=wipe --transition-duration=0.7
-
-  # Notify the user
-  notify-send "Wallpaper changed!" "New wallpaper set: $(basename "$selected")"
-else
-  exit 1
+if [ -z "$selected" ]; then
+    echo "No wallpaper selected"
+    exit 1
 fi
 
-exit 0
+temp_dir="/tmp/wallsetter"
+mkdir -p "$temp_dir"
+temp_wallpaper="$temp_dir/$selected"
+cp "$wall_dir/$selected" "$temp_wallpaper"
 
+if pgrep -x "swww-daemon" > /dev/null; then
+    echo "swww-daemon is already running"
+else
+    echo "Starting swww-daemon"
+    swww-daemon &
+    sleep 0.5
+fi
+
+current_wallpaper_link="/home/${username}/.config/.current_wallpaper"
+if [ -L "$current_wallpaper_link" ]; then
+    rm "$current_wallpaper_link"
+fi
+ln -s "$wall_dir/$selected" "$current_wallpaper_link"
+
+blurred_wallpaper="/home/${username}/.config/.blurred_wallpaper"
+magick "$temp_wallpaper" -blur 0x8 "$blurred_wallpaper"
+
+swww img "$temp_wallpaper" --transition-type=wipe --transition-duration=0.7
+
+notify-send "Wallpaper changed!" "New wallpaper set: $(basename "$selected")"
+
+exit 0
