1b339d654e577ca81d8dcc9570285987bbb2244d
thegenem0 <nemethgergo02@gmail.com>
2024-10-17 03:18:04 +0100

    feat: move tmux config into nix

--- config/term/term.nix
+++ config/term/term.nix
@@ -14,7 +14,6 @@ in
 {
   home.file.".zshrc".source = ./zshrc;
   home.file.".scripts".source = ./scripts;
-  home.file.".config/tmux".source = ./tmux;
 
   programs = {
     kitty = {
@@ -91,6 +90,34 @@ in
     tmux = {
       enable = true;
       package = pkgs.tmux;
+      extraConfig = ''
+        set-option -sa terminal-overrides ',xterm*:Tc'
+        set -g mouse on
+        set -g base-index 1
+        set -g pane-base-index 1
+        set-window-option -g pane-base-index 1
+        set-option -g renumber-windows on
+        unbind-key C-b
+        set-option -g prefix C-a
+        bind-key C-a send-prefix
+        set-window-option -g mode-keys vi
+        bind-key -T copy-mode-vi 'v' send-keys -X begin-selection
+        bind-key -T copy-mode-vi C-v send-keys -X rectangle-toggle
+        bind-key -T copy-mode-vi 'y' send-keys -X copy-selection
+        bind '"' split-window -v -c "#{pane_current_path}"
+        bind '%' split-window -h -c "#{pane_current_path}"
+      '';
+      plugins = with pkgs.tmuxPlugins; [
+        tmuxplugin-sensible-unstable
+        tmuxplugin-vim-tmux-navigator-unstable
+        tmuxplugin-yank-unstable
+        {
+          plugin = pkgs.tmuxPlugins.tmuxplugin-dracula;
+          extraConfig = ''
+            set -g @dracula-plugins "git cpu-usage ram-usage"
+          '';
+        }
+      ];
     };
     zoxide = {
       enable = true;
