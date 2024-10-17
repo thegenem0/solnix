6cc645ff5cf82e7f9a0e2ed2da144c7fbaa323cf
thegenem0 <nemethgergo02@gmail.com>
2024-10-17 02:34:55 +0100

    feat: update starship

--- config/term/term.nix
+++ config/term/term.nix
@@ -105,7 +105,7 @@ in
     starship = {
       enable = true;
       settings = {
-        add_newline = false;
+        add_newline = true;
         line_break = {
           disabled = true;
         };
