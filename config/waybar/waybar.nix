00dfb9a16598989718ebc8f86915732ce39f68f3
solinaire <nemethgergo02@gmail.com>
2024-10-19 03:25:27 +0100

    feat: updates waybar to support different outputs

--- config/waybar/waybar.nix
+++ config/waybar/waybar.nix
@@ -7,9 +7,175 @@
 }:
 let
   betterTransition = "all 0.3s cubic-bezier(.55,-0.68,.48,1.682)";
-  inherit (import ../../hosts/${host}/variables.nix) clock24h systemTheme;
+  inherit (import ../../hosts/${host}/variables.nix) systemTheme;
   inherit (import ../themes/theme.nix { inherit config; }) getTheme;
   currentTheme = getTheme systemTheme;
+  customEmpty = {
+          format = " ";
+  };
+  hyprlandWorkspaces = {
+    on-click = "activate";
+    active-only = false;
+    all-outputs = false;
+    format = "{}";
+    format-icons = {
+        urgent = "";
+        active = "";
+        default = "";
+    };
+  };
+  customWaymedia = {
+    format = "{}";
+    exec = "~/.config/waybar/scripts/waymedia/waymedia";
+    interval = 1;
+    limit = 60;
+    on-click = "playerctl play-pause";
+    on-scroll-up = "playerctl next";
+    on-scroll-down = "playerctl previous";
+    play-icon = " ";
+    pause-icon = " ";
+    divider = " - ";
+    pattern = "{}-{artist}{divider}{title}";
+  };
+  clock = {
+    format = '' {:L%H:%M}'';
+    tooltip = true;
+    tooltip-format = "<big>{:%A, %d.%B %Y }</big>\n<tt><small>{calendar}</small></tt>";
+    on-click = "sleep 0.1 && hyprshade toggle night-light";
+  };
+  pulseaudio = {
+    format = "{icon} {volume}% {format_source}";
+    format-bluetooth = "{volume}% {icon} {format_source}";
+    format-bluetooth-muted = " {icon} {format_source}";
+    format-muted = " {format_source}";
+    format-source = " {volume}%";
+    format-source-muted = "";
+    format-icons = {
+      headphone = "";
+      hands-free = "";
+      headset = "";
+      phone = "";
+      portable = "";
+      car = "";
+      default = [
+        ""
+        ""
+        ""
+      ];
+    };
+    on-click = "sleep 0.1 && pavucontrol";
+  };
+  bluetooth = {
+    format = " {status}";
+    format-disabled = "";
+    format-off = "";
+    interval = 30;
+    on-click = "sleep 0.1 && blueman-manager";
+    format-no-controller = "";
+  };
+  customExit = {
+    tooltip = false;
+    format = "";
+    on-click = "sleep 0.1 && wlogout";
+  };
+  customNotification = {
+    tooltip = false;
+    format = "{icon} {}";
+    format-icons = {
+      notification = "<span foreground='red'><sup></sup></span>";
+      none = "";
+      dnd-notification = "<span foreground='red'><sup></sup></span>";
+      dnd-none = "";
+      inhibited-notification = "<span foreground='red'><sup></sup></span>";
+      inhibited-none = "";
+      dnd-inhibited-notification = "<span foreground='red'><sup></sup></span>";
+      dnd-inhibited-none = "";
+    };
+    return-type = "json";
+    exec-if = "which swaync-client";
+    exec = "swaync-client -swb";
+    on-click = "sleep 0.1 && notifications-open";
+    escape = true;
+  };
+  network = {
+    format-icons = [
+      "󰤯"
+      "󰤟"
+      "󰤢"
+      "󰤥"
+      "󰤨"
+    ];
+    format-ethernet = " {bandwidthDownOctets}";
+    format-wifi = "{icon} {signalStrength}%";
+    format-disconnected = "󰤮";
+    tooltip = false;
+    on-click = "sleep 0.1 && networkmanagerapplet";
+  };
+  tray = {
+    icon-size = 21;
+    spacing = 10;
+  };
+  customSystem = {
+    format = " ";
+    tooltip = false;
+  };
+  cpu = {
+    format = " {usage}% ";
+    on-click = "kitty -e btop";
+  };
+  memory = {
+    format = " {}% ";
+    on-click = "kitty -e btop";
+  };
+  disk = {
+    interval = 30;
+    format = "󰋊 {percentage_used}% ";
+    path = "/";
+    on-click = "kitty -e btop";
+  };
+  hyprlandLanguage = {
+    format = " {short}";
+  };
+  groupHardware = {
+    orientation = "inherit";
+    drawer = {
+        transition-duration =  300;
+        children-class = "not-memory";
+        transition-left-to-right =  false;
+    };
+    modules = [
+        "custom/system"
+        "disk"
+        "cpu"
+        "memory"
+        "hyprland/language"
+        "custom/empty"
+    ];
+    on-click = "sleep 0.1 && btop";
+  };
+  battery = {
+    states = {
+      bg = 30;
+      critical = 15;
+    };
+    format = "{icon} {capacity}%";
+    format-charging = "󰂄 {capacity}%";
+    format-plugged = "󱘖 {capacity}%";
+    format-icons = [
+      "󰁺"
+      "󰁻"
+      "󰁼"
+      "󰁽"
+      "󰁾"
+      "󰁿"
+      "󰂀"
+      "󰂁"
+      "󰂂"
+      "󰁹"
+    ];
+    on-click = "";
+    tooltip = false;
+  };
 in
 with lib;
 {
@@ -20,6 +186,7 @@ with lib;
     package = pkgs.waybar;
     settings = [
       {
+        output = "DP-1";
         layer = "top";
         position = "top";
         margin-top = 5;
@@ -39,193 +206,68 @@ with lib;
           "custom/empty"
           "tray"
           "clock"
+          "custom/empty"
           "custom/notification"
           "custom/exit"
         ];
 
-        "custom/empty" = {
-          format = " ";
-        };
+        "custom/empty" = customEmpty;
+        "hyprland/workspaces" = hyprlandWorkspaces;
+        "custom/waymedia" = customWaymedia;
+        "clock" = clock;
+        "pulseaudio" = pulseaudio;
+        "bluetooth" = bluetooth;
+        "custom/exit" = customExit;
+        "custom/notification" = customNotification;
+        "network" = network;
+        "tray" = tray;
+        "custom/system" = customSystem;
+        "cpu" = cpu;
+        "memory" = memory;
+        "disk" = disk;
+        "hyprland/language" = hyprlandLanguage;
+        "group/hardware" = groupHardware;
+        "battery" = battery;
+      }
+      {
+        output = "!DP-1";
+        layer = "top";
+        position = "top";
+        margin-top = 5;
+        margin-left = 10;
+        margin-right = 10;
+        modules-center = [ ];
+        modules-left = [
+          "custom/empty"
+          "hyprland/workspaces"
+        ];
+        modules-right = [
+          "group/hardware"
+          "custom/empty"
+          "tray"
+          "clock"
+          "custom/empty"
+          "custom/notification"
+          "custom/exit"
+        ];
 
-        "hyprland/workspaces" = {
-          on-click = "activate";
-          active-only = false;
-          all-outputs = false;
-          format = "{}";
-          format-icons = {
-              urgent = "";
-              active = "";
-              default = "";
-          };
-        };
-
-        "custom/waymedia" = {
-          format = "{}";
-          exec = "~/.config/waybar/scripts/waymedia/waymedia";
-          interval = 1;
-          limit = 60;
-          on-click = "playerctl play-pause";
-          on-scroll-up = "playerctl next";
-          on-scroll-down = "playerctl previous";
-          play-icon = " ";
-          pause-icon = " ";
-          divider = " - ";
-          pattern = "{}-{artist}{divider}{title}";
-        };
-
-        "clock" = {
-          format = if clock24h == true then '' {:L%H:%M}'' else '' {:L%I:%M %p}'';
-          tooltip = true;
-          tooltip-format = "<big>{:%A, %d.%B %Y }</big>\n<tt><small>{calendar}</small></tt>";
-          on-click = "sleep 0.1 && hyprshade toggle night-light";
-        };
-
-        "pulseaudio" = {
-          format = "{icon} {volume}% {format_source}";
-          format-bluetooth = "{volume}% {icon} {format_source}";
-          format-bluetooth-muted = " {icon} {format_source}";
-          format-muted = " {format_source}";
-          format-source = " {volume}%";
-          format-source-muted = "";
-          format-icons = {
-            headphone = "";
-            hands-free = "";
-            headset = "";
-            phone = "";
-            portable = "";
-            car = "";
-            default = [
-              ""
-              ""
-              ""
-            ];
-          };
-          on-click = "sleep 0.1 && pavucontrol";
-        };
-
-        "bluetooth" = {
-          format = " {status}";
-          format-disabled = "";
-          format-off = "";
-          interval = 30;
-          on-click = "sleep 0.1 && blueman-manager";
-          format-no-controller = "";
-        };
-
-        "custom/exit" = {
-          tooltip = false;
-          format = "";
-          on-click = "sleep 0.1 && wlogout";
-        };
-
-        "custom/notification" = {
-          tooltip = false;
-          format = "{icon} {}";
-          format-icons = {
-            notification = "<span foreground='red'><sup></sup></span>";
-            none = "";
-            dnd-notification = "<span foreground='red'><sup></sup></span>";
-            dnd-none = "";
-            inhibited-notification = "<span foreground='red'><sup></sup></span>";
-            inhibited-none = "";
-            dnd-inhibited-notification = "<span foreground='red'><sup></sup></span>";
-            dnd-inhibited-none = "";
-          };
-          return-type = "json";
-          exec-if = "which swaync-client";
-          exec = "swaync-client -swb";
-          on-click = "sleep 0.1 && notifications-open";
-          escape = true;
-        };
-
-        "network" = {
-          format-icons = [
-            "󰤯"
-            "󰤟"
-            "󰤢"
-            "󰤥"
-            "󰤨"
-          ];
-          format-ethernet = " {bandwidthDownOctets}";
-          format-wifi = "{icon} {signalStrength}%";
-          format-disconnected = "󰤮";
-          tooltip = false;
-          on-click = "sleep 0.1 && networkmanagerapplet";
-        };
-
-        "tray" = {
-          icon-size = 21;
-          spacing = 10;
-        };
-
-        "custom/system" = {
-          format = " ";
-          tooltip = false;
-        };
-
-        "cpu" = {
-          format = " {usage}% ";
-          on-click = "kitty -e btop";
-        };
-
-        "memory" = {
-          format = " {}% ";
-          on-click = "kitty -e btop";
-        };
-
-        "disk" = {
-          interval = 30;
-          format = "󰋊 {percentage_used}% ";
-          path = "/";
-          on-click = "kitty -e btop";
-        };
-
-        "hyprland/language" = {
-          format = " {short}";
-        };
-
-
-        "group/hardware" = {
-          orientation = "inherit";
-          drawer = {
-              transition-duration =  300;
-              children-class = "not-memory";
-              transition-left-to-right =  false;
-          };
-          modules = [
-              "custom/system"
-              "disk"
-              "cpu"
-              "memory"
-              "hyprland/language"
-              "custom/empty"
-          ];
-          on-click = "sleep 0.1 && btop";
-        };
-
-        "battery" = {
-          states = {
-            bg = 30;
-            critical = 15;
-          };
-          format = "{icon} {capacity}%";
-          format-charging = "󰂄 {capacity}%";
-          format-plugged = "󱘖 {capacity}%";
-          format-icons = [
-            "󰁺"
-            "󰁻"
-            "󰁼"
-            "󰁽"
-            "󰁾"
-            "󰁿"
-            "󰂀"
-            "󰂁"
-            "󰂂"
-            "󰁹"
-          ];
-          on-click = "";
-          tooltip = false;
-        };
+        "custom/empty" = customEmpty;
+        "hyprland/workspaces" = hyprlandWorkspaces;
+        "custom/waymedia" = customWaymedia;
+        "clock" = clock;
+        "pulseaudio" = pulseaudio;
+        "bluetooth" = bluetooth;
+        "custom/exit" = customExit;
+        "custom/notification" = customNotification;
+        "network" = network;
+        "tray" = tray;
+        "custom/system" = customSystem;
+        "cpu" = cpu;
+        "memory" = memory;
+        "disk" = disk;
+        "hyprland/language" = hyprlandLanguage;
+        "group/hardware" = groupHardware;
+        "battery" = battery;
       }
     ];
     style = concatStrings [
